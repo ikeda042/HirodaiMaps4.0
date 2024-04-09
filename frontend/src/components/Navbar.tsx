8abdbdfd2fe850a09d4944fe90af45e1ed38a0a7
ikeda042 <ikeda042main@gmail.com>
2024-04-10 01:41:51 +0900

    feat: navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,20 +3,24 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import QrCodeIcon from '@mui/icons-material/QrCode';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
         <Box >
-            <AppBar sx={{ backgroundColor: '#005e3c', position: "fixed" }}>
-                <Toolbar>
-                    <Box>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', fontFamily: "Helvetica", fontSize: '1.7rem' }}>
-                            HirodaiMaps
-                            <QrCodeIcon />
-                        </Typography>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </Box>
+            <Link to={"/"}>
+                <AppBar sx={{ backgroundColor: '#005e3c', position: "fixed" }}>
+
+                    <Toolbar>
+                        <Box>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', fontFamily: "Helvetica", fontSize: '1.7rem' }}>
+                                HirodaiMaps
+                                <QrCodeIcon />
+                            </Typography>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            </Link>
+        </Box >
     );
 }
