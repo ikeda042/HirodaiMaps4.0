4257d37686cb7176d9d65bccbd29cc666ccb957a
ikeda042 <ikeda042main@gmail.com>
2024-04-10 01:12:12 +0900

    feat:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,17 @@
 import './App.css';
-import { NavBar } from './components/Navbar';
-import BottomNavBar from './components/BottomNav';
-import Box from '@mui/material/Box';
-import BuildingCard from './components/BuildingCard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { TopPage } from './components/TopPage';
 
 function App() {
   return (
-    <Box sx={{ backgroundColor: '#FFFFFF', margin: 0, padding: 0 }}>
-      <NavBar />
-      <Box sx={{ marginTop: '64px', marginBottom: '64px' }}>
-        {Array.from({ length: 10 }, (_, i) => (
-          <BuildingCard key={i} imageUrl='building1.jpg' title={`title${i + 1}`} />
-        ))}
-      </Box>
-      <BottomNavBar />
-    </Box >
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <TopPage />
+        } />
+
+      </Routes>
+    </Router >
   );
 }
 
