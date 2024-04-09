540dcf518121ad1624043d129af97e382c4609c5
ikeda042 <ikeda042main@gmail.com>
2024-04-10 00:48:14 +0900

    feat: app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,18 @@ import './App.css';
 import { NavBar } from './components/Navbar';
 import BottomNavBar from './components/BottomNav';
 import Box from '@mui/material/Box';
+import BuildingCard from './components/BuildingCard';
 
 function App() {
   return (
-    <Box sx={{ backgroundColor: '#FFFFFF', minHeight: '100vh' }}>
+    <Box sx={{ backgroundColor: '#FFFFFF', margin: 0, padding: 0 }}>
       <NavBar />
+      <Box sx={{ marginTop: '64px' }}>
+        <BuildingCard />
+        <BuildingCard />
+      </Box>
       <BottomNavBar />
-    </Box>
+    </Box >
   );
 }
 
