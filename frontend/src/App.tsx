553a6c20119ae46e13caa5c1ea00a77dd9329223
ikeda042 <ikeda042main@gmail.com>
2024-04-10 01:28:30 +0900

    Card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TopPage } from './components/TopPage';
+import BuildingDetail from './components/BuildingDetail';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         <Route path="/" element={
           <TopPage />
         } />
-
+        <Route path="/building/:buildingId" element={
+          <BuildingDetail />
+        } />
       </Routes>
     </Router >
   );
