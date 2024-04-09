6bc94a6c919ac026e6ed7fb3d1b5ca5f945f51c7
ikeda042 <ikeda042main@gmail.com>
2024-04-10 00:24:10 +0900

    feat: Navbar

---
+++ frontend/src/components/BottomNav.tsx
@@ -0,0 +1,32 @@
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import DepartureBoardIcon from '@mui/icons-material/DepartureBoard';
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+import AccessibleIcon from '@mui/icons-material/Accessible';
+import { styled } from '@mui/material/styles';
+
+const CustomBottomNavigationAction = styled(BottomNavigationAction)((theme) => ({
+    borderRight: '1px solid rgba(0, 0, 0, 0.12)',
+    borderLeft: '1px solid rgba(0, 0, 0, 0.12)',
+    '&.Mui-selected': {
+        color: '#005e3c',
+        backgroundColor: '#f0f0f0',
+    }
+}));
+
+const BottomNavBar = () => {
+    return (
+        <BottomNavigation style={{
+            width: '100%',
+            position: 'fixed',
+            bottom: 0,
+            borderTop: '1px solid rgba(0, 0, 0, 0.12)',
+        }}>
+            <CustomBottomNavigationAction label="Departure" icon={<DepartureBoardIcon />} />
+            <CustomBottomNavigationAction label="My Location" icon={<MyLocationIcon />} />
+            <CustomBottomNavigationAction label="Accessibility" icon={<AccessibleIcon />} />S
+        </BottomNavigation>
+    );
+}
+
+export default BottomNavBar;
