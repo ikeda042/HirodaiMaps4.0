cedc97dbaef71805a45bcc2d6e915ed82be6cf38
ikeda042 <ikeda042main@gmail.com>
2024-04-10 00:24:17 +0900

    feat: APP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import { NavBar } from './components/Navbar';
+import BottomNavBar from './components/BottomNav';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
-    <>
+    <Box sx={{ backgroundColor: '#FFFFFF', minHeight: '100vh' }}>
       <NavBar />
-    </>
+      <BottomNavBar />
+    </Box>
   );
 }
 
