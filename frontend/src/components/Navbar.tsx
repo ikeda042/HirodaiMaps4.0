bfeaf0c422ad51a43092aa578d2f9c87e469c94b
ikeda042 <ikeda042main@gmail.com>
2024-04-10 00:00:46 +0900

    feat: navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import QrCodeIcon from '@mui/icons-material/QrCode';
+
+export const NavBar = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{ backgroundColor: '#005e3c' }}>
+                <Toolbar>
+                    <Box>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', fontFamily: "Helvetica", fontSize: '1.7rem' }}>
+                            HirodaiMaps
+                            <QrCodeIcon />
+                        </Typography>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
