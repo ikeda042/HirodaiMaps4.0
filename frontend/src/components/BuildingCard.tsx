304cce82c6f0b4058e0af63f43a6de64f0ec49d2
ikeda042 <ikeda042main@gmail.com>
2024-04-10 00:29:33 +0900

    feat: building card

---
+++ frontend/src/components/BuildingCard.tsx
@@ -0,0 +1,50 @@
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+
+export const BuildingCard = () => {
+
+    return (
+        <div style={{ margin: '0 1%' }}>
+            <Card sx={{
+                width: '100%',
+                height: "90px",
+                display: 'flex',
+                alignItems: 'center',
+                margin: '0.5% auto',
+                borderRadius: "10px"
+            }}>
+                <CardMedia
+                    component="img"
+                    sx={{
+                        width: 100,
+                        height: 75,
+                        padding: '2%',
+                        borderRadius: '15px',
+                        alignSelf: 'center',
+                    }}
+                    image={"a.png"}
+                    alt="Building Image"
+                />
+                <CardContent sx={{ flex: 1 }}>
+                    <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            height: '100%'
+                        }}
+                    >
+                        title2333
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
+
+export default BuildingCard;
