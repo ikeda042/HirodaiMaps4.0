90a23532cc8ae5634cdb2d840657f54cc0851ef8
ikeda042 <ikeda042main@gmail.com>
2024-04-10 16:18:11 +0900

    feat: tt

--- frontend/src/components/BottomNavTop.tsx
+++ frontend/src/components/BottomNavTop.tsx
@@ -4,6 +4,7 @@ import DepartureBoardIcon from '@mui/icons-material/DepartureBoard';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import AccessibleIcon from '@mui/icons-material/Accessible';
 import { styled } from '@mui/material/styles';
+import { Link } from '@mui/material';
 
 const CustomBottomNavigationAction = styled(BottomNavigationAction)((theme) => ({
     borderRight: '1px solid rgba(0, 0, 0, 0.12)',
@@ -22,11 +23,28 @@ const BottomNavBarTop = () => {
             position: 'fixed',
             bottom: 0,
             borderTop: '1px solid rgba(0, 0, 0, 0.12)',
+            display: 'flex',
+            justifyContent: 'space-around',
         }}>
-            <CustomBottomNavigationAction label="Departure" icon={<DepartureBoardIcon />} />
-            <CustomBottomNavigationAction label="My Location" icon={<MyLocationIcon />} />
-            <CustomBottomNavigationAction label="Accessibility" icon={<AccessibleIcon />} />S
-        </BottomNavigation>
+            <Link href="/timetable/1" style={{ display: 'flex' }}>
+                <CustomBottomNavigationAction
+                    label="Departure"
+                    icon={<DepartureBoardIcon />}
+                />
+            </Link>
+            <Link href="/my-location" style={{ display: 'flex' }}>
+                <CustomBottomNavigationAction
+                    label="My Location"
+                    icon={<MyLocationIcon />}
+                />
+            </Link>
+            <Link href="/accessibility" style={{ display: 'flex' }}>
+                <CustomBottomNavigationAction
+                    label="Accessibility"
+                    icon={<AccessibleIcon />}
+                />
+            </Link>
+        </BottomNavigation >
     );
 }
 
