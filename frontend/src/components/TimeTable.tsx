c0079c7a3952e8fe8e729c3f501af303106febd5
ikeda042 <ikeda042main@gmail.com>
2024-04-10 16:11:38 +0900

    TT

--- frontend/src/components/TimeTable.tsx
+++ frontend/src/components/TimeTable.tsx
@@ -1,15 +1,85 @@
-
-
-
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { useParams } from 'react-router-dom';
+import { parse } from 'path';
+import { Box } from '@mui/system';
+import { NavBar } from './Navbar';
+import { useState } from 'react';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { SelectChangeEvent } from '@mui/material/Select';
+import BottomNavBarTop from './BottomNavTop';
 
 const TimeTable = () => {
+
+    const { stopId: initialStopId } = useParams<{ stopId: string }>();
+    const [stopId, setStopId] = useState<string>(initialStopId ?? '1');
+
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        setStopId(event.target.value);
+    };
+
+
+    if (!stopId) {
+        return <div>Invalid stop ID</div>;
+    }
+
+    interface TimeTableData {
+        [key: string]: string[];
+    }
+
+    const timeTableData: TimeTableData = {
+        "1": ['06:23', '06:55', '07:07', '07:32', '08:00', '08:17', '09:07', '09:27', '10:15', '11:00', '11:45', '12:00', '12:30', '12:45', '13:30', '13:45', '14:30', '15:00', '15:15', '15:30', '15:45', '16:20', '16:30', '16:45', '17:00', '17:27', '17:27', '18:10', '18:17', '18:30', '18:55', '19:10', '19:52', '20:12', '21:00', '21:52'],
+        "2": ['9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
+    };
+
+
+    const timeTableElements = timeTableData[stopId];
+
+
+    const now = new Date();
+
+    const futureTimes = timeTableElements.filter(time => {
+        const [hours, minutes] = time.split(':').map(Number);
+        const timeDate = new Date();
+        timeDate.setHours(hours, minutes);
+        return timeDate > now;
+    });
+
     return (
-        <div>
-            <h1>TimeTable</h1>
-        </div>
+        <>
+            <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#FFFFFF', margin: 0, padding: 0 }}>
+                <NavBar />
+                <Select
+                    value={stopId}
+                    onChange={handleChange}
+                    sx={{ marginTop: '80px', marginBottom: '30px', marginLeft: '16px', marginRight: '16px' }}
+                >
+                    <MenuItem value="1">広大中央口から西条駅</MenuItem>
+                    <MenuItem value="2">大学会館前から西条駅</MenuItem>
+                </Select>
+                <Box sx={{ marginTop: '1px', marginBottom: '64px' }}>
+                    <Timeline sx={{ alignItems: 'flex-start' }}>
+                        {futureTimes.map((time, index) => (
+                            <TimelineItem key={index}>
+                                <TimelineSeparator>
+                                    <TimelineDot color={index === 0 ? "success" : "grey"} />
+                                    {index !== futureTimes.length - 1 && <TimelineConnector />}
+                                </TimelineSeparator>
+                                <TimelineContent>{time}</TimelineContent>
+                            </TimelineItem>
+                        ))}
+                    </Timeline>
+                </Box>
+            </Box >
+            <BottomNavBarTop />
+        </>
     )
 }
-
-
 
 export default TimeTable;
