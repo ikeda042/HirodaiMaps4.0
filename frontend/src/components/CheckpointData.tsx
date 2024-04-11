0de3630881e84752f2bd9753944b8c50791bc9dc
ikeda042 <ikeda042main@gmail.com>
2024-04-11 19:43:23 +0900

    TVH

--- frontend/src/components/CheckpointData.tsx
+++ frontend/src/components/CheckpointData.tsx
@@ -1,5 +1,5 @@
 const checkpoints = [
-    { "1": "1", "checkpoint_key": "i", "lat": "34.39933", "lon": "132.71111", "title": "総合科学部" },
+    { "1": "1", "checkpoint_key": "i_b", "lat": "34.39933", "lon": "132.71111", "title": "総合科学部" },
 ];
 
 
