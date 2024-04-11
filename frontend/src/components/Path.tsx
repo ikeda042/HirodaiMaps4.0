475be33ca800069f617da05a2cb625a08b78509d
ikeda042 <ikeda042main@gmail.com>
2024-04-11 17:40:02 +0900

    fix: iframe layout

--- frontend/src/components/Path.tsx
+++ frontend/src/components/Path.tsx
@@ -2,6 +2,7 @@ import { NavBar } from "./Navbar";
 import { Box } from "@mui/system";
 import BottomNavTT from "./BottomNavTT";
 import { useParams } from "react-router-dom";
+import { Stack } from "@mui/material";
 
 
 const Path = () => {
@@ -11,17 +12,19 @@ const Path = () => {
 
     return (
         <>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '71vh' }}>
-                <NavBar />
-                <Box sx={{ width: '100%', maxWidth: '90%', aspectRatio: '1', margin: 'auto', mb: "100px" }}>
-                    <iframe
-                        id="mapIframe"
-                        src={`/routes/${dir}/index.html?lat=${lat}&lon=${lon}`}
-                        title="Map"
-                        style={{ width: '100%', height: height, padding: '0', margin: '0' }}
-                    ></iframe>
-                </Box>
-                <BottomNavTT />
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <Stack spacing={9} alignItems="center" sx={{ width: '100%', maxWidth: '90%', margin: 'auto', mt: "10px" }}>
+                    <NavBar />
+                    <Box sx={{ width: '100%', maxWidth: '98%', }}>
+                        <iframe
+                            id="mapIframe"
+                            src={`/routes/${dir}/index.html?lat=${lat}&lon=${lon}`}
+                            title="Map"
+                            style={{ width: '100%', height: height, padding: '0', margin: '0' }}
+                        ></iframe>
+                    </Box>
+                    <BottomNavTT />
+                </Stack>
             </Box>
         </>
     );
