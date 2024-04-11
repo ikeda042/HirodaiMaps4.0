055072c32630d4e56a50b197b7f3e374900ab2d7
ikeda042 <ikeda042main@gmail.com>
2024-04-12 02:21:53 +0900

    feat: checkpoint id routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
+        <Route path="/:checkpointId" element={
           <TopPage />
         } />
         <Route path="/building/:buildingId" element={
