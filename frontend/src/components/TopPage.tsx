c9e26fbaf2be5ae10dfde0ca0319519e08a34da6
ikeda042 <ikeda042main@gmail.com>
2024-04-12 17:53:30 +0900

    qrid dict

--- frontend/src/components/TopPage.tsx
+++ frontend/src/components/TopPage.tsx
@@ -9,6 +9,7 @@ import { useEffect } from 'react';
 import { Typography } from '@mui/material';
 import buildings from './BuildingData';
 import { useParams } from 'react-router-dom';
+import qridDict from './QRidDict';
 
 type Building = {
     buildingId: string;
@@ -26,17 +27,6 @@ export const TopPage = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const [filteredBuildings, setFilteredBuildings] = useState<Building[]>([]);
     const { checkpointId } = useParams();
-
-    const qridDict: { [key: string]: string } = {
-        '1': '総合科学部支援室前',
-        '2': '教育学部　北第２福利会館前',
-        '3': '中央図書館前',
-        '4': '北第１福利会館前',
-        '5': '中央口交差点',
-        '6': '情報科学部　東福利会館前',
-        '7': '工学部',
-        '8': '大学会館前',
-    };
 
     const handleSearch = (query: string) => {
         setSearchQuery(query);
