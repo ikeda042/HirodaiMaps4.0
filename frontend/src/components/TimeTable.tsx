f3ecabcba19d709058ba33fb2d94fe6dc8e330db
ikeda042 <ikeda042main@gmail.com>
2024-04-14 21:30:26 +0900

    cid tracker

--- frontend/src/components/TimeTable.tsx
+++ frontend/src/components/TimeTable.tsx
@@ -78,7 +78,7 @@ const TimeTable = () => {
                     </Timeline>
                 </Box>
             </Box >
-            <BottomNavBarTT />
+            <BottomNavBarTT checkpointId={checkpointId} />
         </>
     )
 }
