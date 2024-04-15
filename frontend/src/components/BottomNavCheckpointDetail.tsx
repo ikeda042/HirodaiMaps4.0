afaf19e5bc24d069fe2312a07d14f86b6aa908ef
ikeda042 <ikeda042main@gmail.com>
2024-04-15 23:10:53 +0900

    feat: botom nav

---
+++ frontend/src/components/BottomNavCheckpointDetail.tsx
@@ -0,0 +1,57 @@
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import DepartureBoardIcon from '@mui/icons-material/DepartureBoard';
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+import AccessibleIcon from '@mui/icons-material/Accessible';
+import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+
+const CustomBottomNavigationAction = styled(BottomNavigationAction)((theme) => ({
+    borderRight: '1px solid rgba(0, 0, 0, 0.12)',
+    '&.Mui-selected': {
+        color: '#005e3c',
+        backgroundColor: '#f0f0f0',
+    }
+}));
+
+interface BottomNavBarTopProps {
+    checkpointId?: string;
+}
+
+const BottomNavCheckpointDetail = ({ checkpointId }: BottomNavBarTopProps) => {
+    return (
+        <BottomNavigation style={{
+            width: '100%',
+            position: 'fixed',
+            bottom: 0,
+            borderTop: '1px solid rgba(0, 0, 0, 0.12)',
+            display: 'flex',
+            justifyContent: 'space-around',
+        }}>
+            <Link to={`/${checkpointId}`} style={{ display: 'flex', flexGrow: 1, textDecoration: 'none' }}>
+                <CustomBottomNavigationAction
+                    label="建物リスト"
+                    icon={<ApartmentIcon />}
+                    showLabel={true}
+                />
+            </Link>
+            <Link to={`/timetable/1/${checkpointId}`} style={{ display: 'flex', flexGrow: 1, textDecoration: 'none' }}>
+                <CustomBottomNavigationAction
+                    label="時刻表"
+                    icon={<DepartureBoardIcon />}
+                    showLabel={true}
+                />
+            </Link>
+            <Link to="" style={{ display: 'flex', flexGrow: 1, textDecoration: 'none' }}>
+                <CustomBottomNavigationAction
+                    label="近日公開予定"
+                    icon={<AccessibleIcon />}
+                    showLabel={true}
+                />
+            </Link>
+        </BottomNavigation >
+    );
+}
+
+export default BottomNavCheckpointDetail;
