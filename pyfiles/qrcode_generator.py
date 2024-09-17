0193545152816c481e23af0a66b12e261d4b7564
ikeda042 <ikeda042main@gmail.com>
2024-09-17 17:54:01 +0900

    qr

--- pyfiles/qrcode_generator.py
+++ pyfiles/qrcode_generator.py
@@ -1,9 +1,7 @@
-import qrcode             
+import qrcode
 import qrcode.image.pil
 
 
-
-for i in range(1,10):
-    img = qrcode.make(f'https://hirodaimaps.com/{i}') 
-    img.save(f'qr_images/qrcode{i}.png')
-
+for i in range(1, 20):
+    img = qrcode.make(f"https://hirodaimaps.com/{i}")
+    img.save(f"qr_images/qrcode{i}.png")
