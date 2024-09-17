2cee10e5b940dccb6e8057e72ad04eca1afedbfa
ikeda042 <ikeda042main@gmail.com>
2024-09-17 17:54:20 +0900

    QR to 50

--- pyfiles/qrcode_generator.py
+++ pyfiles/qrcode_generator.py
@@ -2,6 +2,6 @@ import qrcode
 import qrcode.image.pil
 
 
-for i in range(1, 20):
+for i in range(1, 50):
     img = qrcode.make(f"https://hirodaimaps.com/{i}")
     img.save(f"qr_images/qrcode{i}.png")
